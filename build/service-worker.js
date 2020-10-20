7dc4c4276c7bb8a16a8d5a7706dd8a47b02ad71c
inukchae <42123333+inukchae@users.noreply.github.com>
2020-10-20 22:19:12 +0900

    Add files via upload

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8f27a45ce4a84e63a0941f6b4c8760c9.js"
+  "/precache-manifest.f6012c33efd2bbcc53ff8073acefa4e1.js"
 );
 
 self.addEventListener('message', (event) => {
